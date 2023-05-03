d0677340e4483480b97b02c9119a23be540b4701
RodriguesIsrael <zbinda956@gmail.com>
2023-05-03 14:59:48 +0000

    defination a method in the program

--- run.py
+++ run.py
@@ -1,17 +1,28 @@
-# print a hundred dashes
-print ("            battleships  ")
-print("   0  1  2  3  4  5  6  7  8  9")
+def show_board(hit,miss,finish):
+    """creating a batllesheep from schratch"""
+    print ("            battleships  ")
+    print("   0  1  2  3  4  5  6  7  8  9")
+         #numbers of rows
 
-hit = [21,22,23]
+    place = 0
+    for x in range(10):
+        #number of collomns 
+        row = ""
+        for y in range(10):
+            ch = " - "
+            if place in miss:
+                ch = " x "
+            elif place in hit:
+                ch =  " o "
+            elif place in finish:
+                ch =  " O " 
 
-place = 0
-for x in range(10): 
-    row = ""
-    for y in range(10):
-        ch = " - "
-        if place in hit:
-            ch = " X "
-        row = row + ch
-        place = place + 1
-    print(x,row)
+            row = row + ch
+            place = place + 1
+        print(x,row)
+
+hit = [21,22]
+miss = [20,24,12,13]
+finish = [23]
+show_board(hit, miss, finish)    
 
