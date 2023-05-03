066d1e9ad16aa8117b0bb76f8a7ec836e2f3c118
RodriguesIsrael <zbinda956@gmail.com>
2023-05-03 11:53:54 +0000

    initial commit

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-# print a hundred dashes 
+# print a hundred dashes
+print ("            battleships  ")
+print("   0  1  2  3  4  5  6  7  8  9")
+
+hit = [21]
+
+place = 0
 for x in range(10): 
-    print("_ "*10)
+    row = ""
+    for y in range(10):
+        ch = " - "
+        if place == 21:
+            ch = " X "
+        row = row + ch
+        place = place + 1
+    print(x,row)
+
