a577474bbf4c2b144c03b03f616903d68aabdcb1
RodriguesIsrael <zbinda956@gmail.com>
2023-05-03 12:00:57 +0000

    added more hits into the hit List

--- run.py
+++ run.py
@@ -2,14 +2,14 @@
 print ("            battleships  ")
 print("   0  1  2  3  4  5  6  7  8  9")
 
-hit = [21]
+hit = [21,22,23]
 
 place = 0
 for x in range(10): 
     row = ""
     for y in range(10):
         ch = " - "
-        if place == 21:
+        if place in hit:
             ch = " X "
         row = row + ch
         place = place + 1
